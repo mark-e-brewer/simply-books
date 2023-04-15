c3431e76787bf9da720ccca41beb369c15854c8e
Mark Brewer <markbrewer369@gmail.com>
2023-04-15 11:06:58 -0500

    setup and started user component

---
+++ components/UserComp.js
@@ -0,0 +1,27 @@
+import React from 'react';
+// import { useRouter } from 'next/router';
+
+export default function UserComp() {
+  // const router = useRouter();
+  // const { firebaseKey } = router.query;
+
+  return (
+    <>
+      <h1>Hello World!</h1>
+    </>
+  );
+}
+
+// UserComp.propTypes = {
+//   image: PropTypes.string,
+//   name: PropTypes.string,
+//   email: PropTypes.string,
+//   lastLogin: PropTypes.string,
+// };
+
+// UserComp.defualtProps = {
+//   image: 'Defualt Image',
+//   name: 'Defualt Name',
+//   email: 'Defualt Email',
+//   lastLogin: 'Defualt Login',
+// };
---
+++ pages/authors.js
@@ -0,0 +1,7 @@
+export default function AuthorsPage() {
+  return (
+    <>
+      <h1>Hello World!</h1>
+    </>
+  );
+}
